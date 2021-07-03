7f85aad817f1165c4c0857c68c81912f12e1aca0
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-07-02 22:29:53 -0700

    tide in the HTML

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -86,6 +88,23 @@ app.get('/api/animals/:id', (req, res) => {
     res.send(404);
   }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
 
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
